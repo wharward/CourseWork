70682cee448ec359ec1cf7cab87af9ac68d00b4c
Aaron Harward <waaronharward@gmail.com>
2016-09-29 13:10:49 -0700

    Assignments Placeholder

---
+++ HTML-CSS/Tutorials/Basic Page/faqTemplate.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template NAme:FAQ Page
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>FAQ's</title>
+		<link rel="stylesheet" type="text/css" href="Login.css">
+			<!--[if lt IE 9]>
+				<script>
+					document.createElement("article");
+					document.createElement("aside");
+					document.createElement("footer");
+					document.createElement("header");
+					document.createElement("main");
+					document.createElement("nav");
+					document.createElement("section");
+				</script>
+			<![endif]-->
+	</head>
+	<body>
+		<h1>Frequently Asked Questions</h1>
+	</body>
+	
